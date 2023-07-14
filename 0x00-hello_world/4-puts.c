470945df6e5a87787042bacc84c457d70a8c302e
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-14 16:18:38 -0700

    hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #!/bin/bash
-#include <stdio.h>
-
-int main() {
-    puts(""\"Programming is like building a multilingual puzzle");
-    return 0;
+#include<stdio.h>
+/**
+ * main - the main function.
+ * Return: 0 for success 1 for program fail
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
