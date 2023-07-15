e067d269de563c5902184323ac46e9bdb05c737b
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-14 18:53:21 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game mandatory

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main{
+	int c = 97;
+
+	while (c <= 122)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
