6f4c02c5df1abc4ae83d92779332d70b361ea129
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-16 16:51:02 -0700

    lowercase except e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: Always 0
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z' c != 'e' && c != q)
+	{
+		putchar(c);
+		c++
+	}
+	putchar('\n');
+	return (0);
+}
