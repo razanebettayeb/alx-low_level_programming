fd791c77f39d55c7547b95956f9ef855b5fa0cda
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-17 16:54:39 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - main function
+ * Return: Always 1 for error
+ */
+int main(void)
+{
+	char *text = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, text, 59);
+	return (1);
+}
