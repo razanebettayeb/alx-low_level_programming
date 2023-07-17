27dd7feb89f4c1e3bb6da1d92a05ff21a95e4e54
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-17 16:00:55 -0700

    hello;printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h> 
 /**
- * main - main function
- * Return: Always 0 for success
- */
-int main() {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+  *main - prints numbers of base 16 in lowercase
+  *Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
