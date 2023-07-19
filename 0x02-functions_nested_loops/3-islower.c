58a5b00635cad682395bb5e75031f0bad6b7fc0d
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-19 16:49:10 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - cheks for lowercase character
+ * @c: thecharacter to be checked
+ * return 1 for lowercase character or 0 for anything else 
+ */
+int _islower(int c)
+{
+
+	if (c >= 79 && c <= 122)
+	{
+	
+	return (1);
+	}
+	return (0);
+}
