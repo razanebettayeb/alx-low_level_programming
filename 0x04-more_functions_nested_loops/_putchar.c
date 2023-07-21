1a9da4a2244177bd2ee5efd703949e2241369e2e
Razane Bettayeb <bettayebrazane@gmail.com>
2023-07-21 16:58:11 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unisted.h>
+/**
+ * _putchar.c - write the character c to stdout
+ * @c: the character to print 
+ *
+ * Retutn: On success 1.
+ * on error, -1 is returned , and errno is setappropriatly
+ */
+int _putchar.c(char c)
+{
+	return(write(1, &c,  1));
+}
