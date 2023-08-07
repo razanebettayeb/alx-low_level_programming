50ab70c852a068c311d1a6b81c76c84e9762f4a5
razanebettayeb <bettayebrazane@gmail.com>
2023-08-07 10:11:37 -0700

    add 0x09-1

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,10 +1,29 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
+/**
+ * _strpbrk - this function that searches a string for any of a set of bytes
+ *
+ * @s: the string target
+ *
+ * @accept: the chat set
+ *
+ * Return: a pointer to the subset first byte
+ */
 char *_strpbrk(char *s, char *accept)
 {
-return strpbrk(s, accept);
+	int i;
+	char *ptr = -1;
 
+	while (*s && ptr == -1)
+	{
+		for (i = 0; accept[i] && ptr == -1; i++)
+		{
+			if (*s == accept[i])
+			{
+				ptr = s;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+}
