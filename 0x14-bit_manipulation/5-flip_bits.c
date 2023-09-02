973f271d91ad474e4980c683d09ba201426411a1
razanebettayeb <bettayebrazane@gmail.com>
2023-09-01 19:27:23 -0700

    bit

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@
  */
  unsigned int flip_bits(unsigned long int n, unsigned long int m)
  {
-	unsigned long int xorval= n ^ m;
+	unsigned long int xorval = n ^ m;
 	unsigned int count = 0;
 
 	while (xorval)
