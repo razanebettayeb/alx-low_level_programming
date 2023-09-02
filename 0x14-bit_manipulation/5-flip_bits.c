07a45beea3f2aa0354ce0454f1737ec6583aac80
razanebettayeb <bettayebrazane@gmail.com>
2023-09-01 19:25:42 -0700

    bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * flip_bits - return number of bits thzt would need to be flipped to
+ * transform one number to another
+ *
+ * @n: first number
+ * @m: second numbea
+ *
+ * Return: number of bits to flips convert numbers
+ */
+ unsigned int flip_bits(unsigned long int n, unsigned long int m)
+ {
+	unsigned long int xorval= n ^ m;
+	unsigned int count = 0;
+
+	while (xorval)
+	{
+		if (xorval & 1ul)
+			count++;
+		xorval = xorval >> 1;
+	}
+	return (count);
+}
